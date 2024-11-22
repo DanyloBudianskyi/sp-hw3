889e9f71af472401db396a5e4d47707db47352bc
Danil <super.danil501@gmail.com>
2024-11-21 18:42:28 -0700

    Add project files.

---
+++ sp-hw3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sp_hw3
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var books = await GetBooksAsync();
+            foreach (var book in books)
+            {
+                Console.WriteLine($"{book.Id} Title: {book.Title}, Author: {book.Author}");
+            }
+            Console.ReadKey();
+        }
+
+        static async Task<List<Book>> GetBooksAsync()
+        {
+            using (var db = new LibraryDBEntities())
+            {
+                return await db.Books.ToListAsync();
+            }
+        }
+    }
+}
